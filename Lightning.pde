1df94e8b19858a9288eb6d625e1b399ce68862e1
unknown <aachow@697-334-Linhir.sf.k12.internal>
2014-09-05 10:51:27 -0700

    added first name to index.html

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,52 @@
+//Aaron Chow
+//AP Computer science Block 3
+
+int startX = 0;
+int startY = 210;
+int endX = 0;
+int endY = 150;
+int colorOne = 0;
+boolean flash = true;
 void setup()
 {
-  size(300,300);
+  size(425,425);
+  strokeWeight(1);
+  frameRate(10);
+  background(0,0,0);
 }
 void draw()
 {
-
+	createLightning();
 }
 void mousePressed()
 {
-
+	if(flash == true)
+	{
+		flash = false;
+		background(0);
+		colorOne = (int)(Math.random()*255);
+	}
+	else if(flash == false)
+		flash = true;
+}
+void createLightning()
+{
+	if(flash == true)
+	{
+	background(0);
+ 	while(endX < 425) //This loop draws a single lightning bolt
+ 		{
+ 			stroke(colorOne,startX - 100,startY - 100);
+ 			endX = startX + (int)(Math.random()*20);
+ 			endY = startY + (int)(Math.random() * 50) - 25;
+ 			line(startX, startY, endX, endY); 	
+ 			startX = endX;
+ 			startY = endY;
+ 		}
+ 	startX = 0;
+	startY = 210;
+	endX = 0;
+	endY = 150;
+ 	}
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,56 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+//Aaron Chow
+//AP Computer science Block 3
+
+int startX = 0;
+int startY = 210;
+int endX = 0;
+int endY = 150;
+int colorOne = 0;
+boolean flash = true;
 public void setup()
 {
-  size(300,300);
-  noLoop();
+  size(425,425);
+  strokeWeight(1);
+  frameRate(10);
+  background(0,0,0);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	createLightning();
 }
 public void mousePressed()
 {
-  redraw();
+	if(flash == true)
+	{
+		flash = false;
+		background(0);
+		colorOne = (int)(Math.random()*255);
+	}
+	else if(flash == false)
+		flash = true;
+}
+public void createLightning()
+{
+	if(flash == true)
+	{
+	background(0);
+ 	while(endX < 425) //This loop draws a single lightning bolt
+ 		{
+ 			stroke(colorOne,startX - 100,startY - 100);
+ 			endX = startX + (int)(Math.random()*20);
+ 			endY = startY + (int)(Math.random() * 50) - 25;
+ 			line(startX, startY, endX, endY); 	
+ 			startX = endX;
+ 			startY = endY;
+ 		}
+ 	startX = 0;
+	startY = 210;
+	endX = 0;
+	endY = 150;
+ 	}
 }
 
   static public void main(String[] passedArgs) {
